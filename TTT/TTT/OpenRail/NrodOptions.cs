43cd5bb4b784e1a9ad96538f68d49df8ec591f89
Christoff Steenkamp <54899515+JacX808@users.noreply.github.com>
2025-11-12 14:41:44 +0000

    feat: Added NrodOption for Openrail connection data

--- TTT/TTT/OpenRail/NrodOptions.cs
+++ TTT/TTT/OpenRail/NrodOptions.cs
@@ -1,6 +1,10 @@
 ﻿namespace TTT.OpenRail;
 
-public class NrodOptions
+public sealed class NrodOptions
 {
-    
+    public string ConnectUrl { get; set; } = "activemq:tcp://publicdatafeeds.networkrail.co.uk:61619?transport.useInactivityMonitor=false&initialReconnectDelay=250&reconnectDelay=500&consumerExpiryCheckEnabled=false";
+    public string? NR_USERNAME { get; set; }     // will come from env/Secrets
+    public string? NR_PASSWORD { get; set; }     // will come from env/Secrets
+    public string[] Topics { get; set; } = new[] { "TRAIN_MVT_ALL_TOC", "VSTP_ALL" };
+    public bool UseDurableSubscription { get; set; } = true;
 }
